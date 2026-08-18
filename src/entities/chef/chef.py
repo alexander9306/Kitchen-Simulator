6f0b404b0d3511d58633633d0721b543f85dd66d
Alexander <alexander9306@gmail.com>
2026-08-18 13:07:31 -0400

    Remove chef and customer files from PR

--- src/entities/chef/chef.py
+++
@@ -1,8 +0,0 @@
-from src.entities.entity.entity import Entity
-
-class Chef(Entity):
-    def __init__(self, name: str, experience: float, created_by: str ) -> None:
-        super().__init__(created_by=created_by)
-
-        self.name = name
-        self.experience = experience
