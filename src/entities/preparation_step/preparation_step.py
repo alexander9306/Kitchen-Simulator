3c6ce60c58618a4e76c589def29483fefda1f927
Alexander <alexander9306@gmail.com>
2026-08-20 11:03:31 -0400

    reshape entities for new Entity

--- src/entities/preparation_step/preparation_step.py
+++ src/entities/preparation_step/preparation_step.py
@@ -1,8 +1,16 @@
 from src.entities.entity.entity import Entity
+from datetime import datetime
 
 class PreparationStep (Entity):
-  def __init__(self, name: str, stepTime: float, created_by: str):
-    super().__init__(created_by=created_by)
+  def __init__(
+    self,
+    id: int,
+    created_by: str,
+    created_at: datetime,
+    name: str,
+    stepTime: float
+  ):
+    super().__init__(created_by=created_by, id=id, created_at=created_at)
 
     self.name = name
     self.stepTime = stepTime
