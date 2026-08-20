abd6fc9a4185e0fe364df9cd3952f51a374856b9
Alexander <alexander9306@gmail.com>
2026-08-20 12:15:48 -0400

    Add id/created_by/created_at to OrderItem constructor

--- src/entities/order/order_item.py
+++ src/entities/order/order_item.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 from src.entities.entity.entity import Entity
 from src.entities.product.product import Product
 
 class OrderItem(Entity):
-    def __init__(self, product: Product, quantity: int, created_by: str, notes: str = "") -> None:
-        super().__init__(created_by=created_by)
+    def __init__(self, id: int, created_by: str, created_at: datetime, product: Product, quantity: int, notes: str = "") -> None:
+        super().__init__(created_by=created_by, id=id, created_at=created_at)
 
         self.product = product
         self.quantity = quantity
